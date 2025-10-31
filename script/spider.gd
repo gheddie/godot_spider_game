503a52d042dec2251835f0679944b854e718d781
Stefan Schulz <s.schulz@sternico.com>
2025-10-31 15:37:38 +0100

    start stop walk animation on buttons

--- script/spider.gd
+++ script/spider.gd
@@ -5,38 +5,25 @@ extends RigidBody3D
 @onready var buttonStartWalking: Button = $GridContainer/StartWalking
 @onready var buttonStopWalking: Button = $GridContainer/StopWalking
 
-# Called when the node enters the scene tree for the first time.
+const WALK_ANIMATION: String = "Animation"
+
 func _ready() -> void:	
 	print("spider is ready...")
 	print(spider_animator)	
 	connect_buttons()	
+	var anim : Animation = spider_animator.get_animation(WALK_ANIMATION)
+	# make walk animation run forever...
+	anim.loop_mode = (Animation.LOOP_LINEAR)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func connect_buttons() -> void:
-	
-	print("connect_buttons...")
-	
 	buttonStartWalking.pressed.connect(start_walking)
 	buttonStopWalking.pressed.connect(stop_walking)
 
-func play_animation(animation_name: String):
-	
-	print("play_animation...")
-	print(animation_name)
-	
-	spider_animator.play(animation_name)
-	
-	pass
-
 func start_walking():
-	
-	print("start_walking...")
-	play_animation("Animation")
+	spider_animator.play(WALK_ANIMATION)
 	
 func stop_walking():
-	
-	print("stop_walking...")
-	pass
+	spider_animator.stop()
