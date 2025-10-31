6a81d8f72f0f4f5fba74e0ec79dbd2a58fed09f9
Stefan Schulz <s.schulz@sternico.com>
2025-10-31 15:21:12 +0100

    export all leg animations as one from blender glb

--- script/spider.gd
+++ script/spider.gd
@@ -2,38 +2,14 @@ extends RigidBody3D
 
 @onready var spider_animator: AnimationPlayer = $spider/AnimationPlayer
 
-@onready var buttonRearLegAnimation: Button = $GridContainer/PlayRearLegAnimation
-@onready var buttonLeftLegAnimation: Button = $GridContainer/PlayLeftLegAnimation
-@onready var buttonRightLegAnimation: Button = $GridContainer/PlayRightLegAnimation
-@onready var buttonFrontLegAnimation: Button = $GridContainer/PlayFrontLegAnimation
-
 @onready var buttonStartWalking: Button = $GridContainer/StartWalking
 @onready var buttonStopWalking: Button = $GridContainer/StopWalking
-
-const LEFT_LEG_ANIM: String = "LeftLegAnimation"
-const RIGHT_LEG_ANIM: String = "RightLegAnimation"
-const FRONT_LEG_ANIM: String = "FrontLegAnimation"
-const REAR_LEG_ANIM: String = "RearLegAnimation"
-
-var frontLegAnimation : Animation
-var rearLegAnimation : Animation
-var leftLegAnimation : Animation
-var rigthLegAnimation : Animation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
 	print("spider is ready...")
 	print(spider_animator)	
 	connect_buttons()	
-	
-	init_animations()
-	
-func init_animations() -> void:
-	
-	frontLegAnimation = spider_animator.get_animation(FRONT_LEG_ANIM)
-	rearLegAnimation = spider_animator.get_animation(REAR_LEG_ANIM)
-	leftLegAnimation = spider_animator.get_animation(LEFT_LEG_ANIM)
-	rigthLegAnimation = spider_animator.get_animation(RIGHT_LEG_ANIM)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -42,11 +18,6 @@ func _process(delta: float) -> void:
 func connect_buttons() -> void:
 	
 	print("connect_buttons...")
-	
-	buttonRearLegAnimation.pressed.connect(play_animation.bind(REAR_LEG_ANIM))
-	buttonFrontLegAnimation.pressed.connect(play_animation.bind(FRONT_LEG_ANIM))
-	buttonLeftLegAnimation.pressed.connect(play_animation.bind(LEFT_LEG_ANIM))
-	buttonRightLegAnimation.pressed.connect(play_animation.bind(RIGHT_LEG_ANIM))
 	
 	buttonStartWalking.pressed.connect(start_walking)
 	buttonStopWalking.pressed.connect(stop_walking)
@@ -63,13 +34,7 @@ func play_animation(animation_name: String):
 func start_walking():
 	
 	print("start_walking...")
-	
-	play_animation(FRONT_LEG_ANIM)
-	play_animation(REAR_LEG_ANIM)
-	play_animation(LEFT_LEG_ANIM)
-	play_animation(RIGHT_LEG_ANIM)
-	
-	pass
+	play_animation("Animation")
 	
 func stop_walking():
 	
