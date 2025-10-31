fb71dc56c07ddc1a36f7cbf4bdd55795e89cb31e
Stefan Schulz <s.schulz@sternico.com>
2025-10-31 13:51:40 +0100

    minor changes

--- script/spider.gd
+++ script/spider.gd
@@ -7,11 +7,33 @@ extends RigidBody3D
 @onready var buttonRightLegAnimation: Button = $GridContainer/PlayRightLegAnimation
 @onready var buttonFrontLegAnimation: Button = $GridContainer/PlayFrontLegAnimation
 
+@onready var buttonStartWalking: Button = $GridContainer/StartWalking
+@onready var buttonStopWalking: Button = $GridContainer/StopWalking
+
+const LEFT_LEG_ANIM: String = "LeftLegAnimation"
+const RIGHT_LEG_ANIM: String = "RightLegAnimation"
+const FRONT_LEG_ANIM: String = "FrontLegAnimation"
+const REAR_LEG_ANIM: String = "RearLegAnimation"
+
+var frontLegAnimation : Animation
+var rearLegAnimation : Animation
+var leftLegAnimation : Animation
+var rigthLegAnimation : Animation
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
 	print("spider is ready...")
 	print(spider_animator)	
-	connect_buttons()
+	connect_buttons()	
+	
+	init_animations()
+	
+func init_animations() -> void:
+	
+	frontLegAnimation = spider_animator.get_animation(FRONT_LEG_ANIM)
+	rearLegAnimation = spider_animator.get_animation(REAR_LEG_ANIM)
+	leftLegAnimation = spider_animator.get_animation(LEFT_LEG_ANIM)
+	rigthLegAnimation = spider_animator.get_animation(RIGHT_LEG_ANIM)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,17 +43,13 @@ func connect_buttons() -> void:
 	
 	print("connect_buttons...")
 	
-	print(buttonRearLegAnimation)
-	buttonRearLegAnimation.pressed.connect(play_animation.bind("RearLegAnimation"))
+	buttonRearLegAnimation.pressed.connect(play_animation.bind(REAR_LEG_ANIM))
+	buttonFrontLegAnimation.pressed.connect(play_animation.bind(FRONT_LEG_ANIM))
+	buttonLeftLegAnimation.pressed.connect(play_animation.bind(LEFT_LEG_ANIM))
+	buttonRightLegAnimation.pressed.connect(play_animation.bind(RIGHT_LEG_ANIM))
 	
-	print(buttonFrontLegAnimation)
-	buttonFrontLegAnimation.pressed.connect(play_animation.bind("FrontLegAnimation"))
-	
-	print(buttonLeftLegAnimation)
-	buttonLeftLegAnimation.pressed.connect(play_animation.bind("LeftLegAnimation"))
-	
-	print(buttonRightLegAnimation)
-	buttonRightLegAnimation.pressed.connect(play_animation.bind("RightLegAnimation"))
+	buttonStartWalking.pressed.connect(start_walking)
+	buttonStopWalking.pressed.connect(stop_walking)
 
 func play_animation(animation_name: String):
 	
@@ -40,4 +58,20 @@ func play_animation(animation_name: String):
 	
 	spider_animator.play(animation_name)
 	
+	pass
+
+func start_walking():
+	
+	print("start_walking...")
+	
+	play_animation(FRONT_LEG_ANIM)
+	play_animation(REAR_LEG_ANIM)
+	play_animation(LEFT_LEG_ANIM)
+	play_animation(RIGHT_LEG_ANIM)
+	
+	pass
+	
+func stop_walking():
+	
+	print("stop_walking...")
 	pass
