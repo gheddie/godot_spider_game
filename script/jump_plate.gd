805a2546a668734d6d30c88a8d9fbb7ecc90013d
Stefan Schulz <s.schulz@sternico.com>
2025-11-05 16:58:07 +0100

    spawning enemies pt1

--- script/jump_plate.gd
+++ script/jump_plate.gd
@@ -4,10 +4,15 @@ extends NavigationPoint
 
 @onready var marker: Node3D = $jumpplate/Marker
 
+@onready var enemy_scene: PackedScene = preload("res://assets/simple_drone.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	StaticPrinter.print("jump plate ready...", self)
 	GameSingleton.get_instance().register_navigation_point(self)
+	
+	spawn_enemy()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,3 +20,6 @@ func _process(delta: float) -> void:
 
 func describe() -> String:
 	return "moo"
+
+func spawn_enemy() -> void:
+	add_child(enemy_scene.instantiate())
