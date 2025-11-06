8c5bd9e4e15a291c3644d08594dd06d8249553f4
Stefan Schulz <s.schulz@sternico.com>
2025-11-06 18:16:43 +0100

    rocket exploding on column obstacle

---
+++ script/column_obstacle.gd
@@ -0,0 +1,18 @@
+class_name ColumnObstacle
+
+extends StaticBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func on_body_entered(body: Node3D) -> void:
+	if body is Rocket:
+		body.explode(get_obstacle_description())
+
+func get_obstacle_description() -> String:
+	return "ColumnObstacle"
--- script/rocket.gd
+++ script/rocket.gd
@@ -1,9 +1,10 @@
+class_name Rocket
+
 extends CharacterBody3D
 
 @onready var shooter: RayCast3D =$Shooter
 
-const SPEED = 40.0
-# const SPEED = 10.0
+const SPEED = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
@@ -13,3 +14,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	position += transform.basis * Vector3(-SPEED,0,0) * delta
 	pass
+
+func explode(sender: String) -> void:
+	StaticPrinter.print(str("rocket exploding........................", sender), self)
+	queue_free()
