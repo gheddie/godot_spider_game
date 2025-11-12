10c1cd55e0f4d3d6dcb462cbd5d554468fd88691
Stefan Schulz <s.schulz@sternico.com>
2025-11-12 12:26:40 +0100

    animation tree pt1

--- script/spider_with_mulitple_animation_players.gd
+++ script/spider_with_mulitple_animation_players.gd
@@ -5,8 +5,8 @@ extends CharacterBody3D
 @onready var player_orig: AnimationPlayer = $AnimationPlayer
 @onready var other_player: AnimationPlayer = $TheOtherAnimationPlayer
 
-@onready var player1: AnimationPlayer = AnimationPlayer.new()
-@onready var player2: AnimationPlayer = AnimationPlayer.new()
+@onready var playerStab: AnimationPlayer = AnimationPlayer.new()
+@onready var playerWalk: AnimationPlayer = AnimationPlayer.new()
 
 var stab_copy: Animation
 var walk_copy: Animation
@@ -14,29 +14,32 @@ var walk_copy: Animation
 const STAB_COPY_KEY = "STAB_COPY_KEY"
 const WALK_COPY_KEY = "WALK_COPY_KEY"
 
+const STAB_LIB_KEY = "StabLib"
+const WALK_LIB_KEY = "WalkLib"
+
 func _ready() -> void:	
 	
 	print(player_orig.get_animation_library_list())
 	print(other_player.get_animation_library_list())
 
-	var orig_lib = player_orig.get_animation_library("")
-	print(orig_lib)
+	var orig_lib = player_orig.get_animation_library("")	
+	print(orig_lib.get_animation_list())
 	
 	stab_copy = player_orig.get_animation("StabAction").duplicate(true)
 	walk_copy = player_orig.get_animation("FrontLegAnimation").duplicate(true)
 	
-	print(stab_copy)
-	print(walk_copy)
+	# print(stab_copy)
+	# print(walk_copy)
 	
-	player1.add_animation_library("MOO", AnimationLibrary.new())
-	player1.get_animation_library("MOO").add_animation(STAB_COPY_KEY, stab_copy)
-	print(player1.get_animation_library_list())
-	print(player1.get_animation_library("MOO").get_animation_list())
+	playerStab.add_animation_library(STAB_LIB_KEY, orig_lib)
+	playerStab.get_animation_library(STAB_LIB_KEY).add_animation(STAB_COPY_KEY, stab_copy)
+	# print(playerStab.get_animation_library_list())
+	# print(playerStab.get_animation_library("MOO").get_animation_list())
 	
-	player2.add_animation_library("MOO", AnimationLibrary.new())
-	player2.get_animation_library("MOO").add_animation(WALK_COPY_KEY, walk_copy)	
-	print(player2.get_animation_library_list())
-	print(player2.get_animation_library("MOO").get_animation_list())
+	playerWalk.add_animation_library(WALK_LIB_KEY, orig_lib)
+	playerWalk.get_animation_library(WALK_LIB_KEY).add_animation(WALK_COPY_KEY, walk_copy)	
+	# print(playerWalk.get_animation_library_list())
+	# print(playerWalk.get_animation_library("MOO").get_animation_list())
 	
 	pass
 	
@@ -50,13 +53,21 @@ func stab() -> void:
 	print("stab")
 	# player1.play("MOO/&STAB_COPY_KEY")
 	# player1.play("MOO/&WALK_COPY_KEY")
-	player_orig.play("StabAction")
+	# player_orig.play("StabAction")
+	print(playerStab.get_animation_library_list())
+	print(playerStab.get_animation_library(STAB_LIB_KEY).get_animation_list_size())
+	print(playerStab.get_animation_library(STAB_LIB_KEY).get_animation_list())
+	playerStab.play(StringName("StabLib/STAB_COPY_KEY"))
 	
 func walk() -> void:
 	print("walk")
 	# player2.play("MOO/&STAB_COPY_KEY")
 	# player2.play("MOO/&WALK_COPY_KEY")
-	other_player.play("stabbing_spider_anim_lib/FrontLegAnimation")
+	# other_player.play("stabbing_spider_anim_lib/FrontLegAnimation")
+	print(playerWalk.get_animation_library_list())
+	print(playerWalk.get_animation_library(WALK_LIB_KEY).get_animation_list_size())
+	print(playerWalk.get_animation_library(WALK_LIB_KEY).get_animation_list())
+	playerWalk.play(StringName("WalkLib/WALK_COPY_KEY"))
 
 func _physics_process(delta: float) -> void:
 	pass
